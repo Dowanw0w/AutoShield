8ae99d01d6cb6dc4c69719f2d4eb3ab5f9231191
Dowan <46449568+Dowanw0w@users.noreply.github.com>
2023-02-17 18:10:03 +0800

    Update DmConfig.cs

--- Config/DmConfig.cs
+++ Config/DmConfig.cs
@@ -15,12 +15,12 @@
         /// <summary>
         /// 大漠插件注册码
         /// </summary>
-        public const string DmRegCode = "jv965720b239b8396b1b7df8b768c919e86e10f";
+        public const string DmRegCode = "";
 
         /// <summary>
         /// 大漠插件版本附加信息
         /// </summary>
-        public const string DmVerInfo = "jsbzr82evgahd97";
+        public const string DmVerInfo = "";
 
         /// <summary>
         /// 大漠插件全局路径,设置了此路径后,所有接口调用中,相关的文件都相对于此路径. 比如图片,字库等.
